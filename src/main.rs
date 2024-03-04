f4a53f2eb4178d630fa97139a4a34379e4790253
Nawasan Wisitsingkhon <nawasan.dev@gmail.com>
2024-03-04 13:07:43 +0700

    update comment

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,12 @@ use panic_halt as _;
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
+    // set PWM-10 as output and high.
     let mut led = pins.d10.into_output();
     led.set_high();
 
     loop {
+        // basic turn off and turn on LCD
         delay_ms(300);
         led.set_low();
         delay_ms(300);
